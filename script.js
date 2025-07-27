c9777ae74de5e4725fdbf2bd1ea8e21e8afd74d1
BenOsso <163864730+BenOsso@users.noreply.github.com>
2025-07-27 13:34:23 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensaje() {
+  const mensaje = document.getElementById('mensaje');
+  mensaje.innerHTML = "Eres la persona más linda que he conocido 💗<br>Gracias por estar a mi lado 🌹";
+}
